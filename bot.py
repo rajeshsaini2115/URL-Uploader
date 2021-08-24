3a89884f53107664f024a1cb6b7f805fc3e2c981
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-08-24 10:13:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,8 +9,7 @@ import os
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
-else:
-    from config import Config
+
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
